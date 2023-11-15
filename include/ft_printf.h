d2f9b53aeb5581544655e65ed6d623fe7a1fd062
Santi Guzman <sguzman@cbr11s1.42barcelona.com>
2023-11-15 19:58:03 +0100

    Nice giu

--- include/ft_printf.h
+++ include/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: sguzman <sguzman@student.42barcelo>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 12:43:58 by sguzman           #+#    #+#             */
-/*   Updated: 2023/11/14 16:34:25 by sguzman          ###   ########.fr       */
+/*   Updated: 2023/11/15 19:47:46 by sguzman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,7 @@ int		ft_printf(const char *format, ...);
 int		ft_putstr(char *str);
 int		ft_putchar(char c);
 void	ft_handle_conversions(char *format, va_list arg, char **str);
+void	ft_append_char(char **str, char c);
+void	ft_append_str(char **str, const char *suffix);
 
 #endif
